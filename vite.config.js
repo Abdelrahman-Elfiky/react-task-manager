0d01b3a38cad354c607dd728e60b9343a6f7c709
Abdelrahman-Elfiky <abdelrahman.elfiky152@gmail.com>
2025-10-30 21:28:15 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 💡 القيمة الصحيحة: اسم المستودع بالضبط
+
 const repoName = 'react-task-manager'; 
 
 export default defineConfig({
   plugins: [react()],
   
-  // يضمن أن جميع المسارات (assets) تبدأ باسم المستودع
+  
   base: `/${repoName}/`, 
 })
